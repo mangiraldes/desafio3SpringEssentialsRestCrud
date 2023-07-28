42f8edfff40c6e3485ec9abeb6f70e7bbec6ca25
mangiraldes <manogiraldes@gmail.com>
2023-07-28 17:48:10 -0300

    initial bundle

---
+++ src/main/java/com/giraldes/curso/devsuperior/serviceRest/ServiceRestApplication.java
@@ -0,0 +1,13 @@
+package com.giraldes.curso.devsuperior.serviceRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giraldes/curso/devsuperior/serviceRest/ServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giraldes.curso.devsuperior.serviceRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
